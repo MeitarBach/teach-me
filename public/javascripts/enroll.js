e18496aa9ee8953618898ab3118b658d1801ffa1
MeitarBach <meitar.bach666@gmail.com>
2020-08-29 14:44:12 +0300

    started enroll backend

--- public/javascripts/enroll.js
+++ public/javascripts/enroll.js
@@ -6,6 +6,22 @@ function displayFileName() {
     });
 }
 
-function submit(){
+async function register(){
+    const proficiencies = document.getElementById("proficiencies").value;
+    const details = document.getElementById("details").value;
     
+    const teacher = {proficiencies, details};
+    console.log(teacher);
+
+
+    let response = await fetch('/enroll', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify(teacher)
+      });
+      
+      let result = await response.json();
+      alert(result.message);
 }
--- routes/enroll.js
+++ routes/enroll.js
@@ -1,9 +1,23 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const redisClient = require('../redis/redisConnector');
 
 /* GET enrollment page. */
 router.get('/', function(req, res) {
   res.render('enroll');
+});
+
+router.post('/', async (req, res) =>{
+  const teacher = req.body;
+  try {
+    
+    console.log(`Teacher details: ${JSON.stringify(teacher)}`);
+    let response = await redisClient.lpush('teachers', JSON.stringify(teacher));
+  } catch (err) {
+    res.status(500)
+  }
+  
+  
 });
 
 module.exports = router;
