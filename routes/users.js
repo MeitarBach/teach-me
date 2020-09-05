891ec87334abeca234f333feb9010549a48de7f3
MaiElenberg <mai11mai@gmail.com>
2020-09-06 01:35:27 +0300

    activity log updates

--- routes/users.js
+++ routes/users.js
@@ -31,6 +31,7 @@ router.get('/', checkSignIn, async (req, res, next) => {
 
 router.get('/activity-log/:id', checkSignIn, async (req, res, next) => {
   const userID = req.params.id;
+  const userPurchases = req.session.user.purchaseHistory;
   try {
     let user = await redisClient.hget('users', userID);
     user = JSON.parse(user);
@@ -42,7 +43,7 @@ router.get('/activity-log/:id', checkSignIn, async (req, res, next) => {
             userCart = JSON.parse(userCart);
         }
 
-    res.render('activityLog', {user:user, userCart : userCart});
+    res.render('activityLog', {user : user, userCart : userCart, userPurchases: userPurchases});
 
   } catch (err) {
     next(err);
