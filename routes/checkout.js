33497b8c46c22f9bd7e486fa4592d5faff21e120
MeitarBach <meitar.bach666@gmail.com>
2020-09-05 17:59:30 +0300

    checkout almost completed

--- routes/checkout.js
+++ routes/checkout.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const shortid = require('shortid');
 const checkSignIn = require('../controllers/session');
 const redisClient = require('../redis/redisConnector');
 const DButils = require('../controllers/utilities');
 
-/* GET store page. */
+/* GET checkout page. */
 router.get('/', checkSignIn, async (req, res, next) => {
     const userID = req.session.user.id;
     console.log(`User ${userID} is in checkout...`);
@@ -24,6 +25,55 @@ router.get('/', checkSignIn, async (req, res, next) => {
         error.log(err.message);
         next(err);
     }
+});
+
+/* POST order details */
+router.post('/', checkSignIn, async (req, res, next) =>{
+    const user = req.session.user;
+    console.log(`User ${user.id} confirmed his purchase...`);
+
+    try {
+        // Get user's shopping cart
+        let userCart = await redisClient.hget('carts', user.id);
+        if (!userCart){
+            userCart = {items:[], totalPrice: 0};
+        } else {
+            userCart = JSON.parse(userCart);
+        }
+
+        // Add cart to user's purchase history
+        userCart.checkoutDate = new Date().toUTCString().slice(0, -7);
+        userCart.orderID = shortid.generate();
+        user.purchaseHistory.unshift(userCart);
+        await redisClient.hset('users', user.id, JSON.stringify(user));
+        console.log(`Added user's cart to his purchase history:`);
+        console.log(user.purchaseHistory);
+
+
+        // Save order with payment details to redis
+        const order = {
+            userID: user.id,
+            cardHolder: req.body.cardHolder,
+            cardNumber: req.body.cardNumber,
+            expirationDate: req.body.expirationDate,
+            cvv: req.body.cvv
+        }
+        Object.assign(order, userCart);
+        await redisClient.hset('orders', order.orderID, JSON.stringify(order));
+        console.log(`Sucssessfully saved order to redis:`);
+        console.log(order);
+
+        // Empty user's cart
+        userCart = {items:[], totalPrice: 0};
+        await redisClient.hset('carts', user.id, JSON.stringify(userCart));
+        console.log(`Successfully emptied user's cart`);
+
+        // Send response to user
+        res.status(200).send({message: 'Succesfully completed your order! Enjoy your lessons!'});
+    } catch (err) {
+        error.log(err);
+        next(err);
+    } 
 });
 
 module.exports = router;
--- test.js
+++ test.js
@@ -1,6 +1,24 @@
-const date = new Date();
-const year = date.getFullYear();
-const month = `0${date.getMonth() + 1}`.slice(-2);
-console.log(`${year}-${month}`);
+const order = {
+    cardHolder: 'req.body.cardHolder',
+    cardNumber: 'req.body.cardNumber',
+    expirationDate: 'req.body.expirationDate',
+    cvv: 'req.body.cvv'
+}
 
-console.log(new Date().getUTCMonth());
+const userCart = {
+    items: [
+        'Thu, 03 Sep 2020 21:04',
+        'Thu, 03 Sep 2020 21:25',
+        'Fri, 04 Sep 2020 10:48',
+        'Fri, 04 Sep 2020 11:26',
+        'Fri, 04 Sep 2020 19:12',
+        'Fri, 04 Sep 2020 19:41',
+        'Sat, 05 Sep 2020 13:01'
+    ],
+    totalPrice: 60
+}
+
+Object.assign(order, userCart);
+
+console.log(order);
+console.log(userCart);
