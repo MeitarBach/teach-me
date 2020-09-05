03e2117597b2513b9d503d4bce8dddc390177c3d
MaiElenberg <mai11mai@gmail.com>
2020-09-05 18:54:26 +0300

    purchse history update

--- routes/purchaseHistory.js
+++ routes/purchaseHistory.js
@@ -5,27 +5,9 @@ const checkSignIn = require('../controllers/session');
 
 /* GET my Purchases page. */
 router.get('/', function(req, res) {
-  res.render('purchaseHistory');
-});
-
-router.get('/activity-log/:id', checkSignIn, async (req, res, next) => {
-  const userID = req.session.user.id;
-    try {
-        let userPurchases = await redisClient.hget('purchases', userID);
-        if (!userCart){
-            userCart = {items:[], totalPrice: 0};
-        } else {
-            userCart = JSON.parse(userCart);
-        }
-
-        console.log(userCart);
-
-        res.render('cart', {userPurchases});
-    } catch (err) {
-        error.log(err.message);
-        next(err);
-    }
-
+  const userPurchases = req.session.user.purchaseHistory;
+  
+  res.render('purchaseHistory', {userPurchases});
 });
 
 module.exports = router;
