a9b549399101e4853d274437f0dbfc5fab237455
MaiElenberg <mai11mai@gmail.com>
2020-09-07 15:58:44 +0300

    purchase history of specified user in activitylog corrected

--- test.js
+++ test.js
@@ -1,9 +1,47 @@
-const DButils = require('./controllers/utilities');
+const fetch = require('node-fetch');
 
-
-async function test(){
-    let users = await DButils.getSetValues('users');
-    console.log(users);
+const URL = "http://localhost:3000"
+const user1 = {
+    firstName : "Aria",
+    lastName : "Stark",
+    email : "aria@stark.com",
+    password : "123",
 }
 
-test();
+async function register() {
+    try {
+        const response = await fetch(URL + '/register', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        });
+        if (response.ok) {
+            debug('Successfully ')
+        }
+    }
+}
+
+try {
+    const response = await fetch(URL + '/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(user)
+    });
+
+    const result = await response.json();
+    
+    if (response.status === 409) {
+        document.getElementById("emailTaken").innerHTML = result.message;
+    } else if (response.status === 500) {
+        throw new Error("There was an error on the server");
+    } else {
+        window.location.href = "store";
+    }
+
+} catch (err) {
+    console.log(err);
+}
